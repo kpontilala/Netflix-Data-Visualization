ef54f6132d8fa97f32848089379106ff37c202fc
Emmanuel Chigozie Nwokeocha <111549474+kpontilala@users.noreply.github.com>
2024-09-01 02:17:00 +0100

    Update BAN 6420 Module 4.R

--- BAN 6420 Module 4.R
+++ BAN 6420 Module 4.R
@@ -2,12 +2,13 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
-
+#read in csv file
 Rnetflix <- read.csv('CleanedNetflixData.csv')
 
+#preview the csv file
 head(Rnetflix,2)
 
-
+#visualize the ratings distribution using ggplot
 ggplot(Rnetflix, aes(x = rating)) +
   geom_bar(fill = "#060270") +
   labs(title = "DISTRIBUTION OF RATINGS", x = "Ratings", y = "Number of Ratings")
